bc03f490f755ba80a26289d2d2ab1025f0c4d47d
viethan <vietaing@gmail.com>
2020-05-27 16:17:45 +0000

    added

---
+++ defcamp_r100/solve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.8.2
+import angr
+
+r = angr.Project('r100')
+
+desired_addr = 0x004007a1
+wrong_addr = 0x00400790
+
+entry_state = r.factory.entry_state(args=["./fairlight"])
+simulation = r.factory.simulation_manager(entry_state)
+simulation.explore(find = desired_addr, avoid = wrong_addr)
+
+solution = simulation.found[0].posix.dumps(0)
+print (solution)
